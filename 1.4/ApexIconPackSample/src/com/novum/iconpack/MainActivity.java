93ae17ccbca97ad11faa1a4fd3a9567d432d9b5c
Johannes Dahlgren <lucifer54321@gmail.com>
2013-02-24 19:37:36 +0100

    Första uppladdning
    V1.4 är första versionen på github

---
+++ 1.4/ApexIconPackSample/src/com/novum/iconpack/MainActivity.java
@@ -0,0 +1,28 @@
+package com.novum.iconpack;
+
+import android.app.Activity;
+import android.os.Bundle;
+//import android.content.ActivityNotFoundException;
+//import android.content.Intent;
+//import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        final String ACTION_SET_THEME = "com.anddoes.launcher.SET_THEME";
+//        final String EXTRA_PACKAGE_NAME = "com.anddoes.launcher.THEME_PACKAGE_NAME";
+//
+//        Intent intent = new Intent(ACTION_SET_THEME);
+//        intent.putExtra(EXTRA_PACKAGE_NAME, getPackageName());
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        try {
+//            startActivity(intent);
+//        } catch (ActivityNotFoundException e) {
+//            Toast.makeText(this, "Apex Launcher is not installed!", Toast.LENGTH_SHORT).show();
+//        }
+//        finish();
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-NovumIcons
-==========
+Swedroid Communitys Ikonpaket - Novum Icons
+
+Initiativet grundar sig på att inga ikonpaket är kompletta, och att det alltid saknas en eller flera ikoner. Ingen PSD finns tillgänglig så man kan inte komplettera med de ikoner man själv saknar. 
+
+Lösningen är naturligtvis att skapa ett ikonprojekt där det finns en PSD för projektet, där många bidrar, och där man snabbt kan skapa de ikoner som man saknar. Allt samlas upp i en projekt-mapp där alla ikoner som är i linje med projektets ramar och design hamnar!
+
+Idén är enkel. Vi ska skapa sjukt mycket ikoner, och förhoppningsvis kan det bli ett riktigt stort ikonpaket som kan utmana de stora drakarna!
+
+För att bidra med ikoner:
+http://www.swedroid.se/forum/showthread.php?t=90177
+
+För frågor och support om appen:
+http://www.swedroid.se/forum/showthread.php?t=90865
