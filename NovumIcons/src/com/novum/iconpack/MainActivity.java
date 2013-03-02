4bb7ecd6c06071c725053f035e31a7ad45121ae5
Johannes Dahlgren <lucifer54321@gmail.com>
2013-03-02 18:12:10 +0100

    småpill
    inget vettigt i denna commit

--- NovumIcons/src/com/novum/iconpack/MainActivity.java
+++ NovumIcons/src/com/novum/iconpack/MainActivity.java
@@ -11,18 +11,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        final String ACTION_SET_THEME = "com.anddoes.launcher.SET_THEME";
-//        final String EXTRA_PACKAGE_NAME = "com.anddoes.launcher.THEME_PACKAGE_NAME";
-//
-//        Intent intent = new Intent(ACTION_SET_THEME);
-//        intent.putExtra(EXTRA_PACKAGE_NAME, getPackageName());
-//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//        try {
-//            startActivity(intent);
-//        } catch (ActivityNotFoundException e) {
-//            Toast.makeText(this, "Apex Launcher is not installed!", Toast.LENGTH_SHORT).show();
-//        }
-//        finish();
+
     }
 
 }
