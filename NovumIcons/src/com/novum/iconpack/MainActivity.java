ef51df12dd37160ecc3cac33334fdd1168bcd5f9
johda876 <johda876@student.liu.se>
2013-07-24 22:24:25 +0200

    Uppdaterat till version 2.0. Strukturerat om koden så man nu använder paketnamn som filnamn, vilket kommer göra framtida jobb mycket enklare.

--- NovumIcons/src/com/novum/iconpack/MainActivity.java
+++ NovumIcons/src/com/novum/iconpack/MainActivity.java
@@ -2,9 +2,6 @@ package com.novum.iconpack;
 
 import android.app.Activity;
 import android.os.Bundle;
-//import android.content.ActivityNotFoundException;
-//import android.content.Intent;
-//import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
